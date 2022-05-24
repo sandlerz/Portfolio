d2eb52178bc45211e376b4f88b99036b3d0cb5c5
sebastian <sandlerzop@gmail.com>
2022-05-24 00:35:46 -0500

    Remove

--- src/Components/AboutName.jsx
+++
@@ -1,9 +0,0 @@
-export default function AboutName() {
-  return (
-    <div className="AboutName">
-      <span className="AboutName__welcome">Hola Welcome 👋,</span>
-      <h1 className="AboutName__name">I’m Sebastian Mesa</h1>
-      <span className="AboutName__software">Front End Engineer</span>
-    </div>
-  )
-}
