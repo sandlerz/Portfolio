7ece00c95d47145ebc10c7260f05ad8dad5f777a
sebastian <sandlerzop@gmail.com>
2022-05-24 00:58:10 -0500

    making form funcionality

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,13 +1,19 @@
 export default function ContactForm() {
   const handleSubmit = event => {
-    event.preventDefault()
+    // event.preventDefault()
   }
   return (
-    <form className="ContactForm" onSubmit={handleSubmit}>
-      <input type="text" placeholder="Name" />
-      <input type="text" placeholder="Email" />
-      <input type="text" placeholder="Subject" />
-      <textarea placeholder="Message" />
+    <form
+      className="ContactForm"
+      onSubmit={handleSubmit}
+      name="contact-form"
+      method="POST"
+      data-netlify="true"
+    >
+      <input type="hidden" name="form-name" value="contact-form" />
+      <input type="text" placeholder="Name" name="name" />
+      <input type="text" placeholder="Email" name="email" />
+      <textarea placeholder="Message" name="massage" />
       <button type="submit">Send Message</button>
     </form>
   )
--- src/components/PortfolioHeader.jsx
+++ src/components/PortfolioHeader.jsx
@@ -3,7 +3,7 @@ import ArrowIcon from '../images/right-arrow.png'
 export default function PortfolioHeader() {
   return (
     <header className="PortfolioHeader__header">
-      <h2>My Porfolio</h2>
+      <h2>My Portfolio</h2>
       <div className="PortfolioHeader__header__arrow AboutHeader__talk">
         <span className="AboutHeader__talk__text">See All</span>
         <div className="AboutHeader__talk__img">
